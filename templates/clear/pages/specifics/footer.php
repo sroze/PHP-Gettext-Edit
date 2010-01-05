ecae687ad8a79715886770985e38ddaa4952af05
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-05 19:18:21 +0000

    Patch: add scripts and jQuery script
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@1509 d88eb640-f151-4234-b64e-622b90e6817c

--- templates/clear/pages/specifics/footer.php
+++ templates/clear/pages/specifics/footer.php
@@ -1,3 +1,4 @@
 <div id="footer"><a href="http://www.d-sites.com/projets/gettextedit/">GetTextEdit</a> &copy; 2009 <a href="http://www.d-sites.com">Samuel ROZE</a></div>
+<script type="text/javascript" src="<?php echo LOCAL_PATH; ?>templates/<?php echo $_CONFIG['template']; ?>/scripts/global.js"></script>
 </body>
 </html>
---
+++ templates/clear/script/global.js
@@ -0,0 +1,16 @@
+/**
+ * Callback for template type <select> changes.
+ * 
+ * @param select_object
+ * @return void
+ */
+function templateTypeChange (select_object)
+{
+	var idx = select_object.selectedIndex; 
+	 // get the value of the selected option 
+	var which = select_object.options[idx].value; 
+	
+	if (which == '@other@') {
+		$('div#other_type').slideDown();
+	}
+}
