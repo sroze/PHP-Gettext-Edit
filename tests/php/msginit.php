c2a84aadecc32cd3eb1c005a057f1cbb1d564d3a
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-05 18:08:50 +0000

    Test using "no-translator"
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@1492 d88eb640-f151-4234-b64e-622b90e6817c

--- tests/php/msginit.php
+++ tests/php/msginit.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PATH', realpath(dirname(__FILE__)).'/');
 
-$exec = exec('msginit -i "'.ROOT_PATH.'file.pot" -o "'.ROOT_PATH.'file.po"');
+$exec = exec('msginit -i "'.ROOT_PATH.'file.pot" -o "'.ROOT_PATH.'file.po" --no-translator');
 var_dump($exec);
 ?>
