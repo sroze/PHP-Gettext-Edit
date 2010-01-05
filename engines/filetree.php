879ecb09e724a0d4c3ce8571d4ffcd04b43fea9e
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-05 23:03:16 +0000

    Patch: print if a directory is empty
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@1526 d88eb640-f151-4234-b64e-622b90e6817c

--- engines/filetree.php
+++ engines/filetree.php
@@ -11,8 +11,9 @@ $root = $project->get('project_path').$project->get('project_languages_path');
 if( file_exists($root . $_POST['dir']) ) {
 	$files = scandir($root . $_POST['dir']);
 	natcasesort($files);
+	echo "<ul class=\"jqueryFileTree\" style=\"display: none;\">";
+	
 	if( count($files) > 2 ) { /* The 2 accounts for . and .. */
-		echo "<ul class=\"jqueryFileTree\" style=\"display: none;\">";
 		// All dirs
 		foreach( $files as $file ) {
 			if( file_exists($root . $_POST['dir'] . $file) && $file != '.' && $file != '..' && is_dir($root . $_POST['dir'] . $file) ) {
@@ -26,8 +27,11 @@ if( file_exists($root . $_POST['dir']) ) {
 				echo "<li><input type=\"checkbox\" name=\"scan_files[]\" value=\"" . htmlentities($_POST['dir'] . $file) . "\" /><a href=\"#\" rel=\"" . htmlentities($_POST['dir'] . $file) . "\" class=\"file ext_$ext\">" . htmlentities($file) . "</a></li>";
 			}
 		}
-		echo "</ul>";	
+	} else {
+		echo '<li>Vide</li>';
 	}
+	
+	echo "</ul>";	
 }
 
 ?>
