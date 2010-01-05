003231e36277d9cc673571e39ec0144fad73a1e9
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-05 18:51:08 +0000

    Test: without path
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@1504 d88eb640-f151-4234-b64e-622b90e6817c

--- tests/php/msginit.php
+++ tests/php/msginit.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PATH', realpath(dirname(__FILE__)).'/');
 
-$command = '/usr/bin/msginit --input="'.ROOT_PATH.'file.pot" --output="'.ROOT_PATH.'file.po" --locale=fr_FR';
+$command = 'msginit --input="'.ROOT_PATH.'file.pot" --output="'.ROOT_PATH.'file.po" --locale=fr_FR';
 $exec = exec($command);
 var_dump($command, $exec);
 ?>
