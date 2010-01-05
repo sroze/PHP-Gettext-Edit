a8426dacb20a0f69d9e9bdf5ece7152575338254
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-05 18:56:41 +0000

    Test: without "locale" but with "output-file" arg instead of "output" which is a bad argu
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@1507 d88eb640-f151-4234-b64e-622b90e6817c

--- tests/php/msginit.php
+++ tests/php/msginit.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PATH', realpath(dirname(__FILE__)).'/');
 
-$command = 'msginit --input="'.ROOT_PATH.'file.pot" --output="'.ROOT_PATH.'file.po" --locale=fr_FR';
+$command = 'msginit --input="'.ROOT_PATH.'file.pot" --output-file="'.ROOT_PATH.'file.po"';
 $exec = exec($command);
 var_dump($command, $exec);
 ?>
