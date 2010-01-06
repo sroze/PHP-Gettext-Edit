057aba6d710fc2795eb889bc6e6afa9ca5299cb1
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-06 15:32:23 +0000

    Patch: create back link
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@1585 d88eb640-f151-4234-b64e-622b90e6817c

--- templates/clear/pages/language-new-po-file.php
+++ templates/clear/pages/language-new-po-file.php
@@ -9,7 +9,7 @@ if (!isset($language)) {
 		<p>À venir</p>
 	</div>
 	<div id="contents" class="with_sidebar">
-		<h1><a href="index.php?page=project&project=<?php echo $project->get('project_id'); ?>"><?php echo $project->get('project_name'); ?></a> &raquo; <?php echo $language->getName(); ?> &raquo; Nouveau fichier .po</h1>
+		<h1><a href="index.php?page=project&project=<?php echo $project->get('project_id'); ?>"><?php echo $project->get('project_name'); ?></a> &raquo; <a href="index.php?page=language&project=<?php echo $project->get('project_id'); ?>&language=<?php echo $language->getCode(); ?>"><?php echo $language->getName(); ?></a> &raquo; Nouveau fichier .po</h1>
 		<?php 
 		if (isset($_POST['name'])) {
 			if (!preg_match('#^([a-z0-9_-]+)$#i', $_POST['name'])) {
