1092301b354fad01b3743098f9273c8b7ffbcff3
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-06 14:17:34 +0000

    Debugguing: no var_dump
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@1575 d88eb640-f151-4234-b64e-622b90e6817c

--- includes/classes/Project_File.php
+++ includes/classes/Project_File.php
@@ -112,7 +112,6 @@ abstract class Project_File
 					if ($file_contents[$bracket_position-1] != '\\') {
 						$string = trim($string);
 						if ($string != '"' AND !empty($string)) {
-							var_dump($string);
 							$result[] = str_replace('&#147;', '"', $string);
 						}
 						$string = '';
