1ce52b18f12d219c29d27e93f283f3973b4a14eb
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-06 18:06:37 +0000

    Fix: datgrid on document.ready New: language-file-editor
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@1607 d88eb640-f151-4234-b64e-622b90e6817c

---
+++ templates/clear/pages/language-file-editor.php
@@ -0,0 +1,16 @@
+<?php
+if (!isset($language_file)) {
+	echo 'Paramètres URL insuffisants';
+	exit();
+}
+?><div id="page">
+	<div id="contents">
+		<h1><a href="index.php?page=project&project=<?php echo $project->get('project_id'); ?>"><?php echo $project->get('project_name'); ?></a> &raquo; 
+			<a href="index.php?page=language&project=<?php echo $project->get('project_id'); ?>&language=<?php echo $language->getCode(); ?>"><?php echo $language->getName(); ?></a> &raquo; 
+			<a href="index.php?page=language-file&project=<?php echo $project->get('project_id'); ?>&language=<?php echo $language->getCode(); ?>&file=<?php echo $language_file->getName(); ?>"><?php echo $language_file->getName(); ?></a> &raquo; 
+			Édition</h1>
+		<?php
+		require PAGE_DIR.'specifics/po-editor.php';
+		?>
+	</div>
+</div>
--- templates/clear/pages/specifics/po-editor.php
+++ templates/clear/pages/specifics/po-editor.php
@@ -1,31 +1,33 @@
 <table id="po_datagrid"></table>
 <script type="text/javascript">
-$("#po_datagrid").flexigrid({
-	url: '<?php echo LOCAL_PATH; ?>engines/po-edit.php',
-	dataType: 'json',
-	colModel: [
-		{display: 'Fuzzy', name : 'fuzzy', width : 20, sortable : true, align: 'center'},
-		{display: 'Chaine d\'origine', name : 'msgid', width : 240, sortable : true, align: 'left'},
-		{display: 'Traduction', name : 'msgstr', width : 240, sortable : true, align: 'left'}
-	],
-	buttons: [
-		{name: 'Ajouter', bclass: 'add', onpress : test},
-		{name: 'Supprimer', bclass: 'delete', onpress : test},
-		{separator: true},
-		{name: 'Éditer', bclass: 'edit', onpress: test}
-	],
-	searchitems: [
-		{display: 'Chaine d\'origine', name : 'msgid', isdefault: true},
-		{display: 'Traduction', name : 'msgstr'}
-	],
-	sortname: "msgid",
-	sortorder: "asc",
-	usepager: false,
-	title: 'Traductions',
-	useRp: false,
-	showTableToggleBtn: true,
-	width: 500,
-	height: 400
+$(document).ready(function() {
+	$("#po_datagrid").flexigrid({
+		url: '<?php echo LOCAL_PATH; ?>engines/po-edit.php',
+		dataType: 'json',
+		colModel: [
+			{display: 'Fuzzy', name : 'fuzzy', width : 20, sortable : true, align: 'center'},
+			{display: 'Chaine d\'origine', name : 'msgid', width : 240, sortable : true, align: 'left'},
+			{display: 'Traduction', name : 'msgstr', width : 240, sortable : true, align: 'left'}
+		],
+		buttons: [
+			{name: 'Ajouter', bclass: 'add', onpress : test},
+			{name: 'Supprimer', bclass: 'delete', onpress : test},
+			{separator: true},
+			{name: 'Éditer', bclass: 'edit', onpress: test}
+		],
+		searchitems: [
+			{display: 'Chaine d\'origine', name : 'msgid', isdefault: true},
+			{display: 'Traduction', name : 'msgstr'}
+		],
+		sortname: "msgid",
+		sortorder: "asc",
+		usepager: false,
+		title: 'Traductions',
+		useRp: false,
+		showTableToggleBtn: true,
+		width: 500,
+		height: 400
+	});
 });
 function test(com,grid)
 {
