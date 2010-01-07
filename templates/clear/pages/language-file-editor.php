d2645908eb39e79b135d9a9dd7cd1adf580d84b7
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-07 17:40:00 +0000

    new: Create a "compile" link on editor
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@1694 d88eb640-f151-4234-b64e-622b90e6817c

--- templates/clear/pages/language-file-editor.php
+++ templates/clear/pages/language-file-editor.php
@@ -5,6 +5,9 @@ if (!isset($language_file)) {
 }
 ?><div id="page">
 	<div id="contents">
+		<div class="link right">
+			<a class="compile" href="index.php?page=language-file-compile&project=<?php echo $project->get('project_id'); ?>&language=<?php echo $language->getCode(); ?>&file=<?php echo $language_file->getName(); ?>"><?php echo _('Compiler'); ?></a>
+		</div>
 		<h1><a href="index.php?page=project&project=<?php echo $project->get('project_id'); ?>"><?php echo $project->get('project_name'); ?></a> &raquo; 
 			<a href="index.php?page=language&project=<?php echo $project->get('project_id'); ?>&language=<?php echo $language->getCode(); ?>"><?php echo $language->getName(); ?></a> &raquo; 
 			<a href="index.php?page=language-file&project=<?php echo $project->get('project_id'); ?>&language=<?php echo $language->getCode(); ?>&file=<?php echo $language_file->getName(); ?>"><?php echo $language_file->getName(); ?></a> &raquo; 
