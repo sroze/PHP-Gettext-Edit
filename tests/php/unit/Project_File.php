a1eded944fe42c8e99653dbeb3bb2814c491424f
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-07 11:27:47 +0000

    Patch: files fonction in Project_File and create Unit Test
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@1644 d88eb640-f151-4234-b64e-622b90e6817c

---
+++ tests/php/unit/Project_File.php
@@ -0,0 +1,80 @@
+<?php
+define('ROOT_PATH', realpath(dirname(__FILE__)).'/');
+require_once 'Aspects/Errors/UnitTest.php';
+$unittest = new UnitTest();
+
+require_once '../../../includes/classes/Project_File.php';
+
+$file_path = ROOT_PATH.'file_test.po';
+$file_contents = <<<EOF
+msgid ""
+msgstr ""
+"Content-Type: text/plain; charset=UTF-8\n"
+"Last-Translator: Samuel ROZE <samuel.roze@gmail.com>\n"
+
+msgid "test2"
+msgstr "translation2"
+
+# Welcome
+#:file.php:12
+msgid "test"
+msgstr "translation"
+
+msgid "longue, très longue traduction"
+msgstr ""
+"Ça c'est du jamais vu!"
+"Parce que elle est très très longur"
+"voir plus..."
+EOF;
+$puts = file_puts_contents(
+	$file_path,
+	$file_contents
+);
+$unittest->setTestTitle('Initialisation du fichier .po');
+$unittest->mustBeEqual($puts, strlen($file_contents));
+
+class Project_File_Test extends Project_File {
+	public function __construct ($file_path) {
+		$this->file_path = $file_path;
+	}
+}
+$file = new Project_File_Test($file_path);
+
+$unittest->setTestTitle('getHeaders');
+$unittest->mustBeEqual($file->getHeaders(), array(
+	'Content-Type' => 'text/plain; charset=UTF-8',
+	'Last-Translator' => 'Samuel ROZE <samuel.roze@gmail.com>'
+));
+
+
+$unittest->setTestTitle('setHeaders');
+$file->setHeaders(array(
+	'Content-Type' => 'text/plain; charset=UTF-8',
+	'Edited-width' => 'Project_File TEST'
+));
+$unittest->mustBeEqual($file->getHeaders(), array(
+	'Content-Type' => 'text/plain; charset=UTF-8',
+	'Edited-width' => 'Project_File TEST'
+));
+
+
+$unittest->setTestTitle('getMessages');
+$unittest->mustBeEqual($file->getMessages(), array(
+	'test2' => 'translation2',
+	'test' => 'translation',
+	'longue, très longue traduction' => 'Ça c\'est du jamais vu!
+Parce que elle est très très longur
+voir plus...',
+));
+
+
+$unittest->setTestTitle('editMessage');
+$file->editMessage('test', 'translation3');
+$unittest->mustBeEqual($file->getMessages(), array(
+	'test2' => 'translation2',
+	'test' => 'translation3',
+	'longue, très longue traduction' => 'Ça c\'est du jamais vu!
+Parce que elle est très très longur
+voir plus...',
+));
+
