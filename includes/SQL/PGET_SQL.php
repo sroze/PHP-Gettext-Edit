aff3fe06bf7f9b85874bd97abc1473803dca3f8e
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-12 16:54:13 +0000

    New: PGTE_SQL class for SQL requests
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@1981 d88eb640-f151-4234-b64e-622b90e6817c

---
+++ includes/SQL/PGET_SQL.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Abstract class for SQL requests.
+ *
+ * @author Samuel ROZE <samuel.roze@gmail.com>
+ */
+abstract class PGET_SQL
+{
+	/**
+	 * Character which is used to delimit fields in an SQL request
+	 * 
+	 * @var string(1)
+	 */
+	public $fields_delimitor = '"';
+	
+	/**
+	 * An assoc array with requests keys and requests contents.
+	 * 
+	 * @var array
+	 */
+	public $requests;
+	
+	/**
+	 * Return a request, or if it doesn't exists, throw an error.
+	 * 
+	 * @param string $name
+	 * 
+	 * @return string $request
+	 */
+	public function get ($name)
+	{
+		if (isset($this->requests[$name]))
+			return $this->requests[$name];
+		else {
+			throw new PGET_SQL_Exception(
+				sprintf('Request "%s" doesn\'t exists', $name)
+			);
+		}
+	}
+	
+	/**
+	 * Concat more arguments
+	 * 
+	 * @param array $arguments
+	 * 
+	 * @return array [array $names, array $values]
+	 */
+	public function more ($arguments)
+	{
+		$more = array('', '');
+		foreach ($arguments as $title => $value) {
+			$more[0] .= ', '.$this->fields_delimitor.$title.$this->fields_delimitor.'"';
+			$more[1] .= ', \''.$value.'\'';
+		}
+		return $more;
+	}
+}
+
+class PGET_SQL_Exception extends Exception {}
+?>
---
+++ includes/SQL/PGET_SQL_mysql.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'PGET_SQL.php';
+
+/**
+ * SQL requests for MySQL for the PHP-Gettext-Edit
+ *
+ * @author Samuel ROZE <samuel.roze@gmail.com>
+ */
+class PGET_SQL_mysql extends PGET_SQL
+{
+	/**
+	 * Character which is used to delimit fields in an SQL request
+	 * 
+	 * @var string(1)
+	 */
+	public $fields_delimitor = '`';
+	
+	/**
+	 * Associative array for each requests.
+	 * 
+	 * @var array
+	 */
+	public $requests = array(
+		'create_user' => 'INSERT INTO %s (username, password, email) VALUES (\'%s\', \'%s\', \'%s\')'
+	);
+}
+
+?>
