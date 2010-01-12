800d47b660744541e276c0b978d14801933f8d03
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-12 17:48:18 +0000

    Fix: CONNECTED is false if it isn't defined after User::fromCookie and from session.
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@1988 d88eb640-f151-4234-b64e-622b90e6817c

--- includes/configuration/user.php
+++ includes/configuration/user.php
@@ -6,7 +6,9 @@ if (!empty($_SESSION['user_informations'])) {
 	$_USER = new User($_SESSION['user_informations']);
 } else if (!empty($_COOKIE['user'])) {
 	$_USER = User::fromCookie($_COOKIE['user']);
-} else {
+}
+
+if (!defined('CONNECTED')) {
 	define('CONNECTED', false);
 }
 ?>
