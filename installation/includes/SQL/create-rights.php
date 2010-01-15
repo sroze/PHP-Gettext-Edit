e52e4e25fb5beae0224f4ac5020f28fe849613e7
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-15 20:15:40 +0000

    New rights at the installation
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@2004 d88eb640-f151-4234-b64e-622b90e6817c

--- installation/includes/SQL/create-rights.php
+++ installation/includes/SQL/create-rights.php
@@ -1,22 +1,55 @@
 <?php
 Rights_Config::init($sql, $_POST['sql-prefix'].'rights_');
 
-// Creation of users groups
+Rights_Admin::createRightsFromArray(
+	array(
+		'admin_access' => array(
+			'admin_template_update'
+		),
+		'projects_access' => array(
+			'project_create',
+			'project_access' => array(
+				'project_edit',
+				'project_create',
+				'project_delete',
+				'languages_access' => array(
+					'language_create',
+					'language_delete',
+					'language_access' => array(
+						'language_edit',
+						'language_delete',
+						'language_files_access' => array(
+							'language_file_create',
+							'language_file_access' => array(
+								'language_file_delete',
+								'language_file_edit',
+								'language_file_compile',
+								'language_file_update'
+							)
+						)
+					)
+				),
+				'templates_access' => array(
+					'template_create',
+					'template_access' => array(
+						'template_delete',
+						'template_edit',
+						'template_update'
+					)
+				)
+			)
+		)
+	)
+);
+
+// Create groups
 $group_admin = Rights_Admin::createGroup('Administrateur');
-$group_trans = Rights_Admin::createGroup('Traducteur');
-
-// Creation of rights
-$right_admin = Rights_Admin::createRight('administration');
-Rights_Admin::createRight('create_user', $right_admin);
-Rights_Admin::createRight('delete_user', $right_admin);
-Rights_Admin::createRight('edit_user', $right_admin);
-
-$right_project = Rights_Admin::createRight('project');
-Rights_Admin::createRight('create_project', $right_project);
-Rights_Admin::createRight('edit_project', $right_project);
-Rights_Admin::createRight('delete_project', $right_project);
 
 // Grant rights to groups
-Rights_Admin::addGroupRights($group_admin, $right_admin);
+// The rest of rights will be granted on project creation
+Rights_Admin::addGroupRights($group_admin, array(
+	'admin_template_update',
+	'project_create'
+));
 
 ?>
