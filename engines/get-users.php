22ee2bb133315893be63e7e64d427a2935c0dcb3
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-23 21:35:15 +0000

    New: project-users
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@2062 d88eb640-f151-4234-b64e-622b90e6817c

---
+++ engines/get-users.php
@@ -0,0 +1,37 @@
+<?php
+define('ROOT_PATH', realpath(dirname(__FILE__).'/../').'/');
+
+require_once ROOT_PATH.'includes/ini_conf.php';
+require_once ROOT_PATH.'includes/configuration.php';
+
+if (!Rights::check('project_users_access')) {
+	echo 'Forbidden';
+	exit();
+}
+
+if (isset($_GET['project'])) {
+	$users = GTE::getUsersHavingRight('project_access', array(
+		'project' => $_GET['project']
+	));
+	
+	$out = array(
+		'total' => count($users),
+		'rows' => array()
+	);
+	
+	foreach ($users as $informations) {
+		
+		$out['rows'][] = array(
+			'id' => $informations['id'],
+			'cell' => array(
+				$informations['id'],
+				$informations['username'],
+				'<img class="loading" />'
+			)
+		);
+	}
+	
+	echo json_encode($out);
+}
+
+?>
--- templates/clear/pages/project-users.php
+++ templates/clear/pages/project-users.php
@@ -12,6 +12,65 @@ if (!isset($project)) {
 	<div id="contents" class="with_sidebar">
 		<h1><a href="index.php?page=project&project=<?php echo $project->get('project_id'); ?>"><?php echo $project->get('project_name'); ?></a> &raquo; <?php 
 		echo _('Utilisateurs'); ?></h1>
-		
+		<table id="users_datagrid"></table>
 	</div>
 </div>
+<script type="text/javascript">
+$(document).ready(function() {
+	var gridWidth = $('div#contents').width() - 20;
+	var colWidth = (gridWidth - 15) / 3;
+	
+	$("#users_datagrid").flexigrid({
+		url: '<?php echo LOCAL_PATH; ?>engines/get-users.php',
+		dataType: 'json',
+		colModel: [
+			{display: '<?php echo _('ID'); ?>', name : 'id', width : 15, sortable : false, align: 'left'},
+			{display: '<?php echo str_replace('\'', '\\\'', _('Nom d\'utilisateur')); ?>', name : 'username', width: colWidth, sortable : false, align: 'left'},
+			{display: '<?php echo _('Groupes'); ?>', name : 'groups', width : colWidth, sortable : false, align: 'left'},
+			{display: '<?php echo _('Droits supplémentaires'); ?>', name : 'rights', width : colWidth, sortable : false, align: 'left'}
+		],
+		buttons: [
+			{name: '<?php echo _('Ajouter'); ?>', bclass: 'add', position: 'left', onpress : function (a,grid){
+				alert('Open POPUP DHTML');
+			}},
+			{name: '<?php echo _('Supprimer'); ?>', bclass: 'delete', position: 'left', onpress : function (a,grid){
+				if ($('.trSelected',grid).length <= 0) {
+					alert('<?php echo _('Vous devez sélectionner au moins un utilisateur'); ?>');
+				} else {
+					if ($('.trSelected',grid).length > 1) {
+						var string = '<?php echo _('Êtes-vous sur de vouloir supprimer ces %d utilisateurs ?'); ?>';
+					} else {
+						var string = '<?php echo _('Êtes-vous sur de vouloir supprimer cet utilisateur ?'); ?>';
+					}
+					
+					if (confirm(string.replace(/%d/, $('.trSelected',grid).length))) {
+						$("#po_datagrid").editRemove($('.trSelected',grid));
+					}
+				}
+			}},
+			{separator: true, position: 'left'},
+			{name: '<?php echo _('Éditer'); ?> ', bclass: 'edit', position: 'left', onpress: function (a,grid){
+				if ($('.trSelected',grid).length == 0) {
+					alert('<?php echo _('Vous devez sélectionner un utilisateur'); ?>');
+				} else {
+					$('.trSelected:first',grid).dblclick();
+				}
+			}}
+		],
+		searchitems: [
+			{display: '<?php echo str_replace('\'', '\\\'', _('Chaine d\'origine')); ?>', name : 'msgid', isdefault: true},
+			{display: '<?php echo _('Traduction'); ?>', name : 'msgstr'}
+		],
+		params:[
+			{name: 'project', value: '<?php echo $project->get('project_id'); ?>'}
+		],
+		usepager: false,
+		title: '<?php echo _('Utilisateurs'); ?>',
+		useRp: false,
+		showTableToggleBtn: false,
+		width: gridWidth,
+		height: 400
+	});
+});
+</script>
+<div class="clear" />
