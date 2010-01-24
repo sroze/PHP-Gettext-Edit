3762b92656e3c41c19cf589ce4eb90ddc0e525b4
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-24 00:04:12 +0000

    Patch: don't close fancybox at border clic
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@2078 d88eb640-f151-4234-b64e-622b90e6817c

--- templates/clear/pages/project-users.php
+++ templates/clear/pages/project-users.php
@@ -19,7 +19,10 @@ if (!isset($project)) {
 </div>
 <script type="text/javascript">
 $(document).ready(function() {
-	$("a#users_link").fancybox(); 
+	$("a#users_link").fancybox({
+		hideOnOverlayClick: false,
+		hideOnContentClick: false
+	}); 
 	
 	var gridWidth = $('div#contents').width() - 20;
 	var colWidth = (gridWidth - 65) / 3;
