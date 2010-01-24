e1583e8bcd27aa83a33dae21e6dbab9432680552
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-24 00:12:27 +0000

    Trunking dev: add select and test for JS
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@2079 d88eb640-f151-4234-b64e-622b90e6817c

--- templates/clear/pages/project-users-add.php
+++ templates/clear/pages/project-users-add.php
@@ -1,1 +1,9 @@
-<p>Add a new user</p>
+<p><strong>Utilisateur:</strong><br />
+<select name="user">
+	<option value="1">User 1</option>
+</select></p>
+<p><strong></strong></p>
+<input type="submit" />
+<script type="text/javascript">
+alert('..');
+</script>
