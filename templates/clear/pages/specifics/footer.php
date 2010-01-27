a9b18dc7aac56a8c649b7900abdfa5515ed3de9f
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-01-27 18:34:12 +0000

    New: use the "multiple" version of fancybox (which will be developped ;-) )
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@2149 d88eb640-f151-4234-b64e-622b90e6817c

--- templates/clear/pages/specifics/footer.php
+++ templates/clear/pages/specifics/footer.php
@@ -2,7 +2,7 @@
 <script type="text/javascript" src="<?php echo LOCAL_PATH; ?>templates/<?php echo $_CONFIG['template']; ?>/scripts/jquery.json-2.2.min.js"></script>
 <script type="text/javascript" src="<?php echo LOCAL_PATH; ?>templates/<?php echo $_CONFIG['template']; ?>/scripts/jquery.filetree.js"></script>
 <script type="text/javascript" src="<?php echo LOCAL_PATH; ?>templates/<?php echo $_CONFIG['template']; ?>/scripts/jquery.datagrid.js"></script>
-<script type="text/javascript" src="<?php echo LOCAL_PATH; ?>templates/<?php echo $_CONFIG['template']; ?>/scripts/jquery.fancybox-1.2.6.js"></script>
+<script type="text/javascript" src="<?php echo LOCAL_PATH; ?>templates/<?php echo $_CONFIG['template']; ?>/scripts/jquery.fancybox-1.2.6.multiple.js"></script>
 <script type="text/javascript" src="<?php echo LOCAL_PATH; ?>templates/<?php echo $_CONFIG['template']; ?>/scripts/swfobject.js"></script>
 <script type="text/javascript" src="<?php echo LOCAL_PATH; ?>templates/<?php echo $_CONFIG['template']; ?>/scripts/global.js"></script>
 </body>
