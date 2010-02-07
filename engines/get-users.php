1da785be0aa42ae2615288621d61ed078e1f3752
samuel <samuel@d88eb640-f151-4234-b64e-622b90e6817c>
2010-02-07 16:29:56 +0000

    Patch: use for others cases that with project
    git-svn-id: svn://svn.d-sites.com/php-gettext-edit/gettextedit/trunk@2186 d88eb640-f151-4234-b64e-622b90e6817c

--- engines/get-users.php
+++ engines/get-users.php
@@ -10,7 +10,20 @@ session_write_close();
 
 $context = GTE::buildContext($_POST);
 
-if (!Rights::check('project_users_access', $context)) {
+if (array_key_exists('language_file', $context)) {
+	$right = 'language_file_users_';
+} else if (array_key_exists('language', $context)) {
+	$right = 'language_users_';
+} else if (array_key_exists('template', $context)) {
+	$right = 'template_users_';
+} else if (array_key_exists('project', $context)) {
+	$right = 'project_users_';
+} else {
+	echo 'Bad context';
+	exit();
+}
+
+if (!Rights::check($right.'access', $context)) {
 	header('HTTP/1.0 403 Forbidden');
 	header('Status: 403');
 	echo 'Forbidden';
@@ -18,40 +31,32 @@ if (!Rights::check('project_users_access', $context)) {
 }
 
 if ($_POST['query'] == 'select') {
-	if (isset($_POST['project'])) {
-		$users = GTE::getUsersHavingRight('project_access', $context);
+	$users = GTE::getUsersHavingRight($right.'access', $context);
+	
+	$out = array(
+		'total' => count($users),
+		'rows' => array()
+	);
+	
+	foreach ($users as $informations) {
 		
-		$out = array(
-			'total' => count($users),
-			'rows' => array()
+		$out['rows'][] = array(
+			'id' => $informations['id'],
+			'cell' => array(
+				'#'.$informations['id'],
+				$informations['username'],
+				'<span class="icon loading" />',
+				'<span class="icon loading" />'
+			)
 		);
-		
-		foreach ($users as $informations) {
-			
-			$out['rows'][] = array(
-				'id' => $informations['id'],
-				'cell' => array(
-					'#'.$informations['id'],
-					$informations['username'],
-					'<span class="icon loading" />',
-					'<span class="icon loading" />'
-				)
-			);
-		}
-		
-		echo json_encode($out);
-	} else {
-		echo 'Invalid arguments';
 	}
-} else if ($_POST['query'] == 'delete') {
-	if (isset($_POST['project'])) {
-		$user_name = $_POST['msgids'][0];
-		$user_id = GTE::getUserIdFromUsername($user_name);
 		
-		Rights_Admin::removeUserRights($user_id, array('project_access'));
-	} else {
-		echo 'Invalid arguments';
-	}
+	echo json_encode($out);
+} else if ($_POST['query'] == 'delete' && Rights::check($right.'admin', $context)) {
+	$user_name = $_POST['msgids'][0];
+	$user_id = GTE::getUserIdFromUsername($user_name);
+		
+	Rights_Admin::removeUserRights($user_id, array('project_access'));
 } else if ($_POST['query'] == 'select-more') { // Groups & Rights
 	if (isset($_POST['user'])) {
 		$groups_all = Rights_Admin::getUserGroups((int) $_POST['user'], $context);
